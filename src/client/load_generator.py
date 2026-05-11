7c916d91396805fc848697b86e3fd388c66007e0
A.Ramy <ahmedramysaidelnaggar@gmail.com>
2026-05-11 20:10:42 +0300

    Updated load generator

--- src/client/load_generator.py
+++ src/client/load_generator.py
@@ -1,17 +1,21 @@
 import requests
 import threading
 
-def simulate_user(user_id):
-    url = "http://197.39.16.85/infer"
-    query = f"Query {user_id}"
+def simulate_user(query, user_id):
+    url = "http://192.168.184.31/infer"
 
-    response = requests.post(url, json={"query": query})
-    print(response.json())
+    try:
+        response = requests.post(url, json={"query": query})
+        data = response.json()
+        print(
+            f"[User {user_id}] Worker: {data.get('worker_id')} | Latency: {data.get('latency'):.2f}s | Result: {data.get('result', '')[:50]}...")
+    except Exception as e:
+        print(f"[User {user_id}] Error: {e}")
 
 def run_load_test(num_users=1000):
     threads = []
     for i in range(num_users):
-        t = threading.Thread(target=simulate_user, args=(i))
+        t = threading.Thread(target=simulate_user, args=(f"user_id: {i}", i))
         threads.append(t)
         t.start()
 
--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
-from workers.gpu_worker import GPUWorker
 from client.load_generator import run_load_test
 
 def main():
-    # Create GPU workers
-    workers = [GPUWorker(i) for i in range(4)] # simulate 4 GPUs
-
     # Run Simulation
     run_load_test()
 
