62c2e76d23067c38ae9ecd0142bafe4818396d10
A.Ramy <ahmedramysaidelnaggar@gmail.com>
2026-05-12 05:09:36 +0300

    Added GPU code

--- src/client/load_generator.py
+++ src/client/load_generator.py
@@ -1,16 +1,30 @@
 import requests
 import threading
+import time
 
 def simulate_user(query, user_id):
-    url = "http://192.168.184.31/infer"
-
-    try:
-        response = requests.post(url, json={"query": query})
-        data = response.json()
-        print(
-            f"[User {user_id}] Worker: {data.get('worker_id')} | Latency: {data.get('latency'):.2f}s | Result: {data.get('result', '')[:50]}...")
-    except Exception as e:
-        print(f"[User {user_id}] Error: {e}")
+    url = "https://wooing-foe-poker.ngrok-free.dev/infer"
+    headers = {
+        "Content-Type": "application/json",
+        "ngrok-skip-browser-warning": "true"
+    }
+    start = time.time()
+    retries = 3
+    for attempt in range(retries):
+        try:
+            response = requests.post(url, json={"query": query}, headers=headers, timeout=60)
+            if response.status_code == 200:
+                data = response.json()
+                latency = time.time() - start
+                print(f"[User {user_id}] Worker: {data.get('worker_id')} | "
+                      f"Server Latency: {data.get('latency')} | Total Latency: {latency:.2f}s "
+                      f"Reply: {data.get('result')}")
+                return
+            else:
+                print(f"[User {user_id}] Attempt {attempt+1} failed with status {response.status_code}, retrying...")
+        except Exception as e:
+            print(f"[User {user_id}] Attempt {attempt+1} error: {e}, retrying...")
+    print(f"[User {user_id}] All retries failed")
 
 def run_load_test(num_users=1000):
     threads = []
--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ from client.load_generator import run_load_test
 
 def main():
     # Run Simulation
-    run_load_test()
+    run_load_test(num_users=12)
+
 
 if __name__ == '__main__':
     main()
