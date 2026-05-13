e63ecc4996408fd33e4cdf3d5365dc8a3003c15f
A.Ramy <ahmedramysaidelnaggar@gmail.com>
2026-05-14 00:06:53 +0300

    Updated code

--- src/client/load_generator.py
+++ src/client/load_generator.py
@@ -9,22 +9,20 @@ def simulate_user(query, user_id):
         "ngrok-skip-browser-warning": "true"
     }
     start = time.time()
-    retries = 3
-    for attempt in range(retries):
-        try:
-            response = requests.post(url, json={"query": query}, headers=headers, timeout=60)
-            if response.status_code == 200:
-                data = response.json()
-                latency = time.time() - start
-                print(f"[User {user_id}] Worker: {data.get('worker_id')} | "
-                      f"Server Latency: {data.get('latency')} | Total Latency: {latency:.2f}s "
-                      f"Reply: {data.get('result')}")
-                return
-            else:
-                print(f"[User {user_id}] Attempt {attempt+1} failed with status {response.status_code}, retrying...")
-        except Exception as e:
-            print(f"[User {user_id}] Attempt {attempt+1} error: {e}, retrying...")
-    print(f"[User {user_id}] All retries failed")
+
+    try:
+        response = requests.post(url, json={"query": query}, headers=headers, timeout=60)
+        if response.status_code == 200:
+            data = response.json()
+            latency = time.time() - start
+            print(f"[User {user_id}] Worker: {data.get('worker_id')} | "
+                  f"Server Latency: {data.get('latency')} | Total Latency: {latency:.2f}s "
+                  f"Reply: {data.get('result')}")
+            return
+        else:
+            _ = response.status_code
+    except Exception as e:
+        print(f"[User {user_id}] error: {e}, retrying...")
 
 def run_load_test(num_users=1000):
     threads = []
--- src/main.py
+++ src/main.py
@@ -2,8 +2,7 @@ from client.load_generator import run_load_test
 
 def main():
     # Run Simulation
-    run_load_test(num_users=12)
-
+    run_load_test(num_users=100)
 
 if __name__ == '__main__':
     main()
